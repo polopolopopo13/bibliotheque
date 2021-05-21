f4eaab67339c8d2424eb38630ec1fd6c83160f4b
Moi <polopolopopo13@gmail.com>
2021-05-21 10:09:13 +0200

    neww books

--- js/javaS.js
+++ js/javaS.js
@@ -5,15 +5,16 @@ class Livre {
 	  this.reference = reference;
 	}
   }
+
 var reference = 0;
-var list_livres = new Array({"titre":"Dessiner un mouton", "auteur":"E. St. Berger"});
+var list_livres = new Array({"titre":"Dessiner un mouton", "auteur":"E. St. Berger"}, {"titre": "Marche à l'ombre","auteur": "Sauron"}, {"titre": "Mille et une pattes","auteur": "Freudon"});
 var n_livres = list_livres.length;
 
 var bibliotheque = new Array();
 
 for(i=0; i<= n_livres-1; i++){
 	bibliotheque.push(new Livre(list_livres[i].titre, list_livres[i].auteur, reference+1))
-
+	reference ++;
 }
 
 console.log(bibliotheque);
